97ac1216f1cdd0f2d77142439fbe34a2028a75e3
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-04 02:55:25 +0500

    language querying

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import Control.Concurrent.Async
 import Control.Lens
 import Data.Aeson
 import Data.Text (Text)
@@ -27,20 +28,43 @@ newtype TranslateResponse = TranslateResponse
 
 instance FromJSON TranslateResponse
 
+data Language = Language
+  { code :: Text,
+    name :: Text
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Language
+
 main :: IO ()
 main = do
+  lngs <- getLanguages
+  print lngs
+
+  results <- forConcurrently lngs $ \lang -> do
+    rest <- translateText "en" "Haskell is an interesting programming language." (code lang)
+    pure (name lang <> ": " <> rest)
+
+  mapM_ T.putStrLn results
+
+getLanguages :: IO [Language]
+getLanguages = do
+  rsp <- asJSON =<< get "https://translate.terraprint.co/languages"
+  pure (rsp ^. responseBody)
+
+translateText :: Text -> Text -> Text -> IO Text
+translateText sourceLang text targetLang = do
   rsp <-
     asJSON
       =<< post
         "https://translate.terraprint.co/translate"
         ( toJSON
             ( TranslateRequest
-                { q = "This is a test from haskell",
-                  source = "en",
-                  target = "ru",
+                { q = text,
+                  source = sourceLang,
+                  target = targetLang,
                   format = "text"
                 }
             )
         )
-
-  T.putStrLn (translatedText (rsp ^. responseBody))
+  pure (translatedText (rsp ^. responseBody))
