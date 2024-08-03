7f5290d2b9ce9f3dc49fe762b3714e53399a683f
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-04 02:28:27 +0500

    just testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,46 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Lens
+import Data.Aeson
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+import GHC.Generics
 import Network.Wreq
+
+data TranslateRequest = TranslateRequest
+  { q :: Text,
+    source :: Text,
+    target :: Text,
+    format :: Text
+  }
+  deriving (Generic)
+
+instance ToJSON TranslateRequest
+
+newtype TranslateResponse = TranslateResponse
+  { translatedText :: Text
+  }
+  deriving (Generic)
+
+instance FromJSON TranslateResponse
 
 main :: IO ()
 main = do
-  rsp <- get "https://libretranslate.com/languages"
-  print rsp
+  rsp <-
+    asJSON
+      =<< post
+        "https://translate.terraprint.co/translate"
+        ( toJSON
+            ( TranslateRequest
+                { q = "This is a test from haskell",
+                  source = "en",
+                  target = "ru",
+                  format = "text"
+                }
+            )
+        )
+
+  T.putStrLn (translatedText (rsp ^. responseBody))
